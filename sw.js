f8dcba63705fef80fb246a4554c0ee0b8fe145b8
2198764529 <2198764529@users.noreply.github.com>
2023-12-06 10:53:49 +0000

    vault backup: 2023-12-06 18:53:29 2198764529/obsidian@7070d0fda7248792f250793e35ae8fbeff9e47a7

--- sw.js
+++ sw.js
@@ -1,51 +1,51 @@
-// const cacheName = self.location.pathname
-// const pages = [
-// 
-// ];
+const cacheName = self.location.pathname
+const pages = [
 
-// self.addEventListener("install", function (event) {
-//   self.skipWaiting();
+];
 
-//   caches.open(cacheName).then((cache) => {
-//     return cache.addAll(pages);
-//   });
-// });
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
 
-// self.addEventListener("fetch", (event) => {
-//   const request = event.request;
-//   if (request.method !== "GET") {
-//     return;
-//   }
+  caches.open(cacheName).then((cache) => {
+    return cache.addAll(pages);
+  });
+});
 
-//   /**
-//    * @param {Response} response
-//    * @returns {Promise<Response>}
-//    */
-//   function saveToCache(response) {
-//     if (cacheable(response)) {
-//       return caches
-//         .open(cacheName)
-//         .then((cache) => cache.put(request, response.clone()))
-//         .then(() => response);
-//     } else {
-//       return response;
-//     }
-//   }
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") {
+    return;
+  }
 
-//   /**
-//    * @param {Error} error
-//    */
-//   function serveFromCache(error) {
-//     return caches.open(cacheName).then((cache) => cache.match(request.url));
-//   }
+  /**
+   * @param {Response} response
+   * @returns {Promise<Response>}
+   */
+  function saveToCache(response) {
+    if (cacheable(response)) {
+      return caches
+        .open(cacheName)
+        .then((cache) => cache.put(request, response.clone()))
+        .then(() => response);
+    } else {
+      return response;
+    }
+  }
 
-//   /**
-//    * @param {Response} response
-//    * @returns {Boolean}
-//    */
-//   function cacheable(response) {
-//     return response.type === "basic" && response.ok && !response.headers.has("Content-Disposition")
-//   }
+  /**
+   * @param {Error} error
+   */
+  function serveFromCache(error) {
+    return caches.open(cacheName).then((cache) => cache.match(request.url));
+  }
 
-//   event.respondWith(fetch(request).then(saveToCache).catch(serveFromCache));
-// });
+  /**
+   * @param {Response} response
+   * @returns {Boolean}
+   */
+  function cacheable(response) {
+    return response.type === "basic" && response.ok && !response.headers.has("Content-Disposition")
+  }
+
+  event.respondWith(fetch(request).then(saveToCache).catch(serveFromCache));
+});
